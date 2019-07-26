16f1febb0b2879efdf587c4c8f5877cd96294200
Milenko <mlaptosevic@gmail.com>
2019-07-26 22:40:37 +0200

    Added different panels depending on work mode

---
+++ src/components/QuestionPanel/QuestionPanel.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+class QuestionPanel extends React.Component {}
+
+export default QuestionPanel;
--- src/pages/ExamPage.tsx
+++ src/pages/ExamPage.tsx
@@ -6,6 +6,8 @@ import ModelConfigurator from '../components/ModelConfigurator/ModelConfigurator
 import { Col, Container, Row } from 'react-bootstrap';
 import Assignment from '../components/Assignment/Assignment';
 import Diagram from '../components/Diagram/Diagram';
+import QuestionPanel from '../components/QuestionPanel/QuestionPanel';
+import ResultPanel from '../components/ResultPanel/ResultPanel';
 
 interface ExamPageProps {
     workMode: WorkMode;
@@ -37,7 +39,9 @@ class ExamPage extends React.Component<ExamPageProps> {
                         <Diagram />
                     </Col>
                     <Col>
-                        <ModelConfigurator />
+                        {this.props.workMode === WorkMode.WORKING && <ModelConfigurator />}
+                        {this.props.workMode === WorkMode.QUESTIONING && <QuestionPanel />}
+                        {this.props.workMode === WorkMode.FINISHED && <ResultPanel />}
                     </Col>
                 </Row>
             </Container>
